fb50be969235cc5b203380cc60987c8b8d32ac59
asfakn <asfakn459@gmail.com>
2024-07-15 20:29:43 +0530

    Get user Input

---
+++ Java_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Java_1 {
+    public static void main(String[] args) {
+Scanner userinput= new Scanner(System.in);
+System.out.print("Enter the number :");
+int number = userinput.nextInt();
+System.out.print("Your Entered " + number);
+    }
+}
