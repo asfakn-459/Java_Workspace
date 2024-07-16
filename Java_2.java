6a374b49296259e0d89b8e9db056423d737c6d16
asfakn <asfakn459@gmail.com>
2024-07-16 21:26:20 +0530

    add two integer

---
+++ Java_2.java
@@ -0,0 +1,8 @@
+public class Java_2 {
+    public static void main(String[] args) {
+        int firstnumber = 10;
+        int secondnumber = 10;
+        System.out.println(firstnumber + " + " + secondnumber + " = " + (firstnumber+secondnumber));
+
+    }
+}
