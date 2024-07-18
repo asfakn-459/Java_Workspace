77212616ff5026f0665e9f54cf440b58d76eb67b
asfakn <asfakn459@gmail.com>
2024-07-18 21:56:32 +0530

    Ascii Values

---
+++ Java_4.java
@@ -0,0 +1,9 @@
+public class Java_4 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char hc = 'b';
+        int ascii = ch + hc;
+        int castascii = (int) ascii;
+        System.out.println(castascii);
+    }
+}
