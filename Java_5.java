49ee6b442f7e38e51f8b2dd8cd7347dd75e4ecb8
asfakn <asfakn459@gmail.com>
2024-07-19 21:42:12 +0530

    Swap Two Number

---
+++ Java_5.java
@@ -0,0 +1,17 @@
+public class Java_5{
+    public static void main(String args[]) {
+        int n = 10; // Number of terms to print
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.println("Fibonacci Series till " + n + " terms:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(firstTerm + ", ");
+
+            // Compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
