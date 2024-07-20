e8d62f20eaeab50e0432a1e38d5454284c64ac5e
asfakn <asfakn459@gmail.com>
2024-07-20 23:00:57 +0530

    Quotient and Reminder

---
+++ Java_7.java
@@ -0,0 +1,7 @@
+public class Java_7 {
+    public static void main(String[]args){
+        int dividend=25,divisor=4;
+        System.out.println("Quotient :" + dividend / divisor);
+        System.out.println("Reminder :" + dividend % divisor);
+    }
+}
