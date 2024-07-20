8e3e8745c01cb3982a17d7fce3f22fe808c0bf08
asfakn <asfakn459@gmail.com>
2024-07-20 23:07:12 +0530

    Fibonacci Series

---
+++ Java_8.java
@@ -0,0 +1,16 @@
+public class Java_8 {
+    public static void main(String[]args){
+        int a = 50, b = 40;
+        System.out.println("---Before Swap---");
+        System.out.println("First Number :" + a);
+        System.out.println("Second Number:" + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("---After Swap---");
+        System.out.println("First Number :" + a);
+        System.out.println("Second Number:" + b);
+
+
+    }
+}
