6e0215a8bae3453cef72f6821b7a42ec491d4d19
asfakn <asfakn459@gmail.com>
2024-07-21 19:52:22 +0530

    Array Index

---
+++ Java_10.java
@@ -0,0 +1,8 @@
+public class Java_10 {
+    public static void main(String[] args) {
+        String[]array={"asfak","arul","jeeva","paramesh","mani"};
+        System.out.println(array[3]+","+array[4]+","+array[0]+","+array[1]+","+array[2]);
+
+
+    }
+}
